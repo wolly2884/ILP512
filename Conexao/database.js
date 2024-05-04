256dfab1360ad4317037ae7641681f2d8ee390e5
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2024-05-04 18:41:10 -0300

    Update database.js

--- Conexao/database.js
+++ Conexao/database.js
@@ -1,13 +1,17 @@
-// database.js
-const mysql = require('mysql');
+const { Client } = require('pg');
 
 // Configuração da conexão
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "lp4"
+const client = new Client({
+  user: 'root',
+  host: 'cora6ca1hbls73f3a0og-a/lp4',
+  database: 'lp4',
+  password: '5QO9yXRGoSCAHAjQsY4QUKtQUhZN93S0',
+  port: 5432, // Porta padrão do PostgreSQL
+  ssl: { rejectUnauthorized: false } // Necessário para conexão com o Render
 });
 
-module.exports = connection;
+client.connect()
+  .then(() => console.log('Conectado ao banco de dados'))
+  .catch(err => console.error('Erro ao conectar ao banco de dados', err));
+
+module.exports = client;
