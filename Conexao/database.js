0fae53095b5cdfd3c4c1fa87090f559c689769cd
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2024-05-05 19:14:20 -0300

    Update database.js

--- Conexao/database.js
+++ Conexao/database.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 // Configuração da conexão
 const client = new Client({
   user: 'root',
-  host: 'cora6ca1hbls73f3a0og-a/lp4',
+  host: 'cora6ca1hbls73f3a0og-a',
   database: 'lp4',
   password: '5QO9yXRGoSCAHAjQsY4QUKtQUhZN93S0',
   port: 5432, // Porta padrão do PostgreSQL
